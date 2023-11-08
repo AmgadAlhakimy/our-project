f3ec42846ec391693403829a56553ab030af64df
khaledabdunasermansour <Khaledabdulnasermansour@gmail.com>
2023-11-08 23:45:12 +0300

    adding a new pages fonr connecting interfaces

---
+++ resources/views/activities/class-activites.blade.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>class and activites</title>
+    <link rel="stylesheet" href="../../css/all.min.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css.map">
+    <link rel="stylesheet" href="../../css/framework.css">
+    <link rel="stylesheet" href="../../css/style-bottuns.css">
+    <!-- <link rel="stylesheet" href="../../css/master.css"> -->
+</head>
+<body>
+    <main class="main ">
+        <section class="section" >
+            <!-- <h1 class="p-relative title-1">اضافة كلاس</h1> -->
+            <form action="">
+                <h3 class="container-tilte">اضف انشطة للفصول</h3>
+                <div class="container containers-style">
+                    <div class=""> 
+                        <div class="row">
+                            <!-- 1 -->
+                            <div class="box col-12 ">
+                                <label for="level-class">the class</label>
+                                <select class="form-select form-control " id="level-class"> 
+                                    <option  value="">روضة</option>
+                                    <option  value="">ابتدائي</option>
+                                </select>
+                            </div>
+                            <hr>
+                            <!-- 2 -->
+                                <div class="box mb-1">
+                                    <label class="" for="level-class">choose the activites please</label>
+                                </div>
+                                
+                                <div class="box ">
+                                        <div class="btn-container ">
+                                        <!-- <div class="btn-menu "> -->
+                                        <div class="btnl-container  row">
+                                            <!-- -------- start buttons  -->
+                                            <label class="btnl-lable col ">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">السباحة</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">ركوب الخيل</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">الرسم </span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">المهن</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">السلام العالمي</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">الصداقة</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">رحلة للسبعين</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text ">رحلة لمستشفى السعيد
+                                                </span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">رحلة لملعب الوحدة</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">رحلة لحمل</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">يوم الرياضة</span>
+                                            </label>
+                                            <label class="btnl-lable col ">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">السباحة</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">ركوب الخيل</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">الرسم </span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">المهن</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">السلام العالمي</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">الصداقة</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">رحلة للسبعين</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text ">رحلة لمستشفى السعيد
+                                                </span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">رحلة لملعب الوحدة</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">رحلة لحمل</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">يوم الرياضة</span>
+                                            </label>
+                                            <label class="btnl-lable col ">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">السباحة</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">ركوب الخيل</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">الرسم </span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">المهن</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">السلام العالمي</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">الصداقة</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">رحلة للسبعين</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text ">رحلة لمستشفى السعيد
+                                                </span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">رحلة لملعب الوحدة</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">رحلة لحمل</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">يوم الرياضة</span>
+                                            </label>
+                                            <!-- -------- end buttons  -->
+                                        </div>
+                                    </div>
+                                </div>
+                            <!-- </div> -->
+                        </div>
+                            
+
+                            
+                    </div>
+                </div>
+                <div class=" row">
+                    <div class="box ">
+                        <input class="save-button " type="submit" value="Save " >
+                        <input class="clear-button " type="reset" value="clear" >
+                    </div>
+                </div>
+            </form>
+        </div>
+    </main>
+</body>
+</html>
---
+++ resources/views/classes/class-teatcher.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>class and teacher</title>
+    <link rel="stylesheet" href="../../css/all.min.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css.map">
+    <link rel="stylesheet" href="../../css/framework.css">
+    <link rel="stylesheet" href="../../css/style-bottuns.css">
+    <!-- <link rel="stylesheet" href="../../css/master.css"> -->
+</head>
+<body>
+    <main class="main ">
+        <section class="section" >
+            <!-- <h2 class="p-relative title-1">اختر فصول المدرس</h2> -->
+            <form action="">
+                <h3 class="container-tilte">اضف فصول للمدرس</h3>
+                <div class="container containers-style">
+                    <div class=""> 
+                        <div class="row">
+                            <!-- 1 -->
+                            <div class="box col-12 ">
+                                <label for="level-class">the teacher</label>
+                                <select class="form-select form-control " id="level-class"> 
+                                    <option  value="">ahmad</option>
+                                    <option  value="">khalid</option>
+                                    <option  value="">amgad</option>
+                                </select>
+                            </div>
+                            <hr>
+                            <!-- 2 -->
+                                <div class="box mb-1">
+                                    <label class="" for="level-class">choose the classes please</label>
+                                </div>
+                                
+                                <div class="box ">
+                                        <div class="btn-container ">
+                                        <!-- <div class="btn-menu "> -->
+                                        <div class="btnl-container  row">
+                                            <!-- -------- start buttons  -->
+                                            <label class="btnl-lable col ">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">الصف الاول</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">الصف الثاني</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">حضانة </span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">kg1</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">kg2</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">الصف الثالث</span>
+                                            </label>
+                                            <label class="btnl-lable col">
+                                                <input class="light-btn" type="checkbox" >
+                                                <span class="btnl-text">reception </span>
+                                            </label>
+                                        
+                                            <!-- -------- end buttons  -->
+                                        </div>
+                                    </div>
+                                </div>
+                            <!-- </div> -->
+                        </div>
+                            
+
+                            
+                    </div>
+                </div>
+                <div class=" row">
+                    <div class="box ">
+                        <input class="save-button " type="submit" value="Save " >
+                        <input class="clear-button " type="reset" value="clear" >
+                    </div>
+                </div>
+            </form>
+        </div>
+    </main>
+</body>
+</html>
