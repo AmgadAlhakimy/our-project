0bff2ea4e4a9047cede3b9d81c735789025718c0
khaledabdunasermansour <Khaledabdulnasermansour@gmail.com>
2023-11-09 00:26:01 +0300

    adding imiges, css file, sidebar bage, student info tabel, and modifaide framework.css

---
+++ resources/views/students/student-info-table.blade.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/all.min.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/framework.css">
+    <link rel="stylesheet" href="../../css/tables.css">
+</head>
+<body>
+    <div class="my-table">
+        <div class="table-header">
+            <h3>student information</h3>
+            <div class="row">
+                <input class=" col form-control" placeholder="SEARCH">
+                <button class="col-3 add-new">Search</button>
+            </div>
+        </div>
+        <!-- table-hover table-striped -->
+        <div class="card table-section ">
+            <table class=" " >
+                <thead>
+                    <tr>
+                        <th scope="col"><div class="th-head-1  " >id</div></th>
+                        <th scope="col"><div class="th-head-3" >Name</div></th>
+                        <th scope="col"><div class="th-head-2" >photo</div></th>
+                        <th scope="col"><div class="th-head-4" >address 1</div></th>
+                        <th scope="col"><div class="th-head-4" >address 2</div></th>
+                        <th scope="col"><div class="th-head-1" >sex</div></th>
+                        <th scope="col"><div class="th-head-2" >birth date</div></th>
+                        <th scope="col"><div class="th-head-3 " >place of birth</div></th>
+                        <th scope="col"><div class="th-head-3" >date of joining</div></th>
+                        <th scope="col"><div class="th-head-3" >take medicine</div></th>
+                        <th scope="col"><div class="th-head-3" >medicine description</div></th>
+                        <th scope="col"><div class="th-head-3" >have allergy</div></th>
+                        <th scope="col"><div class="th-head-3" >allergy description</div></th>
+                        <th scope="col"><div class="th-head-3" >health problem</div></th>
+                        <th scope="col"><div class="th-head-4 ">health problem description</div> </th>
+                        <th scope="col"><div class="th-head-2" >class</div></th>
+                        <th scope="col"><div class="th-head-4" >note</div></th>
+                        <th scope="col"><div class="th-head-3" >parent</div></th>
+                        <th scope="col"><div class="th-head-2" >processes</div></th>
+                        <th scope="col"><div class="th-head-1" ></div></th>
+                        <th scope="col"><div class="th-head-1" ></div></th>
+                        
+
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>130</td>
+                        <td>mohanad naser mansour</td>
+                        <td><img src="../../images/skills-01.jpg" alt=""></td>
+                        <td>darsalm after the aljaady hosbital</td>
+                        <td>alsafia after the alkhateb resturant</td>
+                        <td>male</td>
+                        <td>12-10-2004</td>
+                        <td>sana'a</td>
+                        <td>12-2-2020</td>
+                        <td>no</td>
+                        <td>none</td>
+                        <td>yes</td>
+                        <td>from same frouts</td>
+                        <td>yes</td>
+                        <td>he can't preth verey will</td>
+                        <td>frist gride</td>
+                        <td>PAKHI GUPTA</td>
+                        <td><button class="save-button btn-info me-5">Parent<i class="ms-5 fa-solid fa-male"></i><i class=" fa-solid fa-female"></i></button></td>
+                        <td><button class="save-button btn-success w-100"><i class="fa-solid fa-pen-to-square"></i></button></td>
+                        <td><button class="clear-button btn-danger w-100"><i class="fa-solid fa-trash"></i></button></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>130</td>
+                        <td>mohanad naser mansour</td>
+                        <td><img src="../../images/skills-01.jpg" alt=""></td>
+                        <td>darsalm after the aljaady hosbital</td>
+                        <td>alsafia after the alkhateb resturant</td>
+                        <td>male</td>
+                        <td>12-10-2004</td>
+                        <td>sana'a</td>
+                        <td>12-2-2020</td>
+                        <td>no</td>
+                        <td>none</td>
+                        <td>yes</td>
+                        <td>from same frouts</td>
+                        <td>yes</td>
+                        <td>he can't preth verey will</td>
+                        <td>frist gride</td>
+                        <td>PAKHI GUPTA</td>
+                        <td><button class="btn save-button btn-info me-5">Parent<i class="ms-5 fa-solid fa-male"></i><i class=" fa-solid fa-female"></i></button></td>
+                        <td><button class="btn save-button btn-success w-100"><i class="fa-solid fa-pen-to-square"></i></button></td>
+                        <td><button class="btn clear-button btn-danger w-100"><i class="fa-solid fa-trash"></i></button></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
+
+<!-- <th scope="col"><div class="th-head-3" >father name</div></th>
+<th scope="col"><div class="th-head-3" >father work</div></th>
+<th scope="col"><div class="th-head-3" >father contact 1</div></th>
+<th scope="col"><div class="th-head-3" >father contact 2</div></th>
+<th scope="col"><div class="th-head-3" >mother name</div></th>
+<th scope="col"><div class="th-head-3" >mother work</div></th>
+<th scope="col"><div class="th-head-3" >mother contact 1</div></th>
+<th scope="col"><div class="th-head-3" >mother contact 2</div></th>
+<th scope="col"><div class="th-head-3" >next to kin</div></th>
+<th scope="col"><div class="th-head-3" >next to kin relationship</div></th>
+<th scope="col"><div class="th-head-3" >next to kin contact</div></th>
+<th scope="col"><div class="th-head-4" >note</div></th>
+<th scope="col"><div class="th-head-2" >processes</div></th>
+<th scope="col"><div class="th-head" > </div></th>
+ -->
+
+
+
+<!-- 
+
+
+<main class=" m-3">
+<table class="table my-card table-hover table-striped ">
+    <thead>
+    <tr>
+        <th scope="col"><div class="th-head-1  " >id</div></th>
+        <th scope="col"><div class="th-head-3" >Name</div></th>
+        <th scope="col"><div class="th-head-2" >photo</div></th>
+        <th scope="col"><div class="th-head-4" >address 1</div></th>
+        <th scope="col"><div class="th-head-4" >address 2</div></th>
+        <th scope="col"><div class="th-head-1" >sex</div></th>
+        <th scope="col"><div class="th-head-2" >birth date</div></th>
+        <th scope="col"><div class="th-head-3 " >place of birth</div></th>
+        <th scope="col"><div class="th-head-3" >date of joining</div></th>
+        <th scope="col"><div class="th-head-3" >take medicine</div></th>
+        <th scope="col"><div class="th-head-3" >medicine description</div></th>
+        <th scope="col"><div class="th-head-3" >have allergy</div></th>
+        <th scope="col"><div class="th-head-3" >allergy description</div></th>
+        <th scope="col"><div class="th-head-3" >health problem</div></th>
+        <th scope="col"><div class="th-head-4 ">health problem description</div> </th>
+        
+        <th scope="col"><div class="th-head-3" >father name</div></th>
+        <th scope="col"><div class="th-head-3" >father work</div></th>
+        <th scope="col"><div class="th-head-3" >father contact 1</div></th>
+        <th scope="col"><div class="th-head-3" >father contact 2</div></th>
+        <th scope="col"><div class="th-head-3" >mother name</div></th>
+        <th scope="col"><div class="th-head-3" >mother work</div></th>
+        <th scope="col"><div class="th-head-3" >mother contact 1</div></th>
+        <th scope="col"><div class="th-head-3" >mother contact 2</div></th>
+        <th scope="col"><div class="th-head-3" >next to kin</div></th>
+        <th scope="col"><div class="th-head-3" >next to kin relationship</div></th>
+        <th scope="col"><div class="th-head-3" >next to kin contact</div></th>
+        <th scope="col"><div class="th-head-4" >note</div></th>
+        <th scope="col"><div class="th-head-2" >processes</div></th>
+        <th scope="col"><div class="th-head" > </div></th>
+
+    </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1 1</td>
+            <td>khalid abdulnasser mansour</td>
+            <td>no photo in this moment </td>
+            <td>darsalm after the aljaady hosbital</td>
+            <td>alsafia after the alkhateb resturant</td>
+            <td>male</td>
+            <td>12-10-2004</td>
+            <td>sana'a</td>
+            <td>12-2-2020</td>
+            <td>no</td>
+            <td>none</td>
+            <td>yes</td>
+            <td>from same frots</td>
+            <td>yes</td>
+            <td>he can't preth verey will</td>
+            <td>ali muhammad</td>
+            <td>taxi driver</td>
+            <td>32423432434</td>
+            <td>234324324</td>
+            <td>fatima</td>
+            <td>house ceber</td>
+            <td>234324324</td>
+            <td>234324434</td>
+            <td>salim mohammed</td>
+            <td>uncle</td>
+            <td>3209409432</td>
+            <td>Lorem ipsum dolor, sit amet </td>
+            <td><a href="{{route('classes.edit',$class->id)}}" class="save-button btn w-100 mt-1">Edit</a></td>
+                <form method="post" action="{{route('classes.destroy',$class->id)}}">
+            <td><button class="btn clear-button w-100 mt-1">Delete</button></td>
+                </form>
+        </tr>
+    </tbody>
+</table>
+</main> -->
