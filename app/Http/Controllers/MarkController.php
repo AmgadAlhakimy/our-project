4ad2e152555fe3ed319e10c8b8e31e3608e54578
AmgadAlhakimy <amgadalhakimy3@gmail.com>
2024-03-20 22:32:35 +0300

    fix conflict

--- app/Http/Controllers/MarkController.php
+++ app/Http/Controllers/MarkController.php
@@ -33,8 +33,9 @@ class MarkController extends Controller
     {
         try {
             $classroom = Classroom::findorfail(1);
-                $marks = Mark::where('classroom_id', $id)
-                                ->where('subject_id', $id)->get();
+                $marks = Mark::where('classroom_id', 1)
+                                ->where('subject_id', 1)->get();
+//                return $marks;
             return view('teachers_affairs/marks.insert_marks',
                 compact('marks','classroom'));
 
--- resources/views/teachers_affairs/marks/insert_marks.blade.php
+++ resources/views/teachers_affairs/marks/insert_marks.blade.php
@@ -2,11 +2,11 @@
 @section('content')
     <?php $counter = 1 ?>
         <!--    --><?php //$month = "2024-01-31" ?><!---->
-    @foreach($marks as $mark)
-            <?php $month = $mark->created_at->format('F') ?>
-            <?php $subject = $mark->subject->name ?>
-            <?php $classroom = $mark->classroom ?>
-    @endforeach
+    {{--    @foreach($marks as $mark)--}}
+    {{--            <?php $month = $mark->created_at->format('F') ?>--}}
+    {{--            <?php $subject = $mark->subject->name ?>--}}
+    {{--            <?php $classroom = $mark->classroom ?>--}}
+    {{--    @endforeach--}}
 
     <div class="my-table">
         {{-- -------***********START THE HEAD OF TABLES***********-------- --}}
@@ -22,7 +22,7 @@
                         <div class="row">
                             <label class="col-10">
                                 <input type="text" required class="form-control " name="search"
-                                    value="{{isset($search) ? $search : ''}}">
+                                       value="{{$search ?? ''}}">
                             </label>
                             <button type="submit" class="col save-button ">{{__('public.search')}}</button>
                         </div>
@@ -37,14 +37,13 @@
                 <div class="cards-container  third-card">
                     <div class="card-info card-info_2 col ">
                         <h4 class=" Names"> {{$classroom->name}}</h4>
-{{--                        <h4 class=" text-center">{{$subject}}</h4>--}}
+                        {{--                        <h4 class=" text-center">{{$subject}}</h4>--}}
                     </div>
 
                     <div class="card-info card-info_2 col ">
                         <h4 class=" text-center">Month</h4>
-{{--                        <h4 class=" Names">{{$month}}</h4>--}}
+                        {{--                        <h4 class=" Names">{{$month}}</h4>--}}
                     </div>
-                </div>
                 </div>
 
                 {{-- -------***********END THE HEAD OF TABLES***********-------- --}}
@@ -103,39 +102,44 @@
                                     {{-- <td><input type="hidden" name="mark[]" value="{{$mark->id}}"></td> --}}
                                     {{-- <td>{{$counter}}</td> --}}
                                     <td>{{$mark->student->id}} 123123</td>
-                                    <td>{{$mark->student->name}} AMGAD ALHAKEMY</td>
+                                    <td>{{$mark->student->name}} AMGAD AL-HAKIMY</td>
                                     <td><img src="{{$mark->student->photo}}" class="student-img" alt=""></td>
-                                    <td><input type="number" value="{{$mark->exam}}" name="exam[]" class=" form-control "
-                                            id='Exam-Mark' placeholder="_" min="0" max="60">
-                                            60
+                                    <td><label for='Exam-Mark'></label><input type="number" value="{{$mark->exam}}"
+                                                                              name="exam[]" class=" form-control "
+                                                                              id='Exam-Mark' placeholder="_" min="0"
+                                                                              max="60">
+
                                         @error('exam')
                                         <small class="form-text text-danger">{{$message}}</small>
                                         @enderror</td>
-                                    <td><input type="number" value="{{$mark->homework}}" name="homework[]"
-                                            class=" form-control "
-                                            id='HW-Mark' placeholder="_" min="0" max="20">
-                                            20
+                                    <td><label for='HW-Mark'></label><input type="number" value="{{$mark->homework}}"
+                                                                            name="homework[]"
+                                                                            class=" form-control "
+                                                                            id='HW-Mark' placeholder="_" min="0"
+                                                                            max="20">
+
                                         @error('homework')
                                         <small class="form-text text-danger">{{$message}}</small>
                                         @enderror</td>
-                                    <td><input type="number" value="{{$mark->oral}}" name="oral[]" class=" form-control "
-                                            id='Oral-Mark' placeholder="_" min="0" max="10">
+                                    <td><label for='Oral-Mark'></label><input type="number" value="{{$mark->oral}}"
+                                                                              name="oral[]" class=" form-control "
+                                                                              id='Oral-Mark' placeholder="_" min="0"
+                                                                              max="10">
 
-                                            7720388293
 
                                         @error('oral')
                                         <small class="form-text text-danger">{{$message}}</small>
                                         @enderror</td>
-                                    <td><input type="number" value="{{$mark->behavior}}" name="behavior[]"
-                                            class=" form-control "
-                                            id='behavior-Mark' placeholder="_" min="0" max="10">
-                                            2020
+                                    <td><label for='behavior-Mark'></label><input type="number"
+                                                                                  value="{{$mark->behavior}}"
+                                                                                  name="behavior[]"
+                                                                                  class=" form-control "
+                                                                                  id='behavior-Mark' placeholder="_"
+                                                                                  min="0" max="10">
+
                                         @error('behavior')
                                         <small class="form-text text-danger"> {{$message}}</small>
                                         @enderror</td>
-                                    <td><label for="">90</label></td>
-                                    <td><label for="">18</label></td>
-                                    <td><label for="">90%</label></td>
                                 </tr>
                                     <?php $counter++ ?>
                             @endforeach
@@ -144,19 +148,18 @@
 
                     </div>
                 </div>
-            <div class=" box row mt-5">
-                <div class=" col mt-1 ">
-                    {{-- زر الترحيل إلى الكنترول --}}
-                    <input class="save-button  " type="button" value="{{__('public.ترحيل')}}">
+                <div class=" box row mt-5">
+                    <div class=" col mt-1 ">
+                        {{-- زر الترحيل إلى الكنترول --}}
+                        <input class="save-button  " type="button" value="{{__('public.ترحيل')}}">
+                    </div>
+                    <div class=" col mt-1">
+                        {{-- زر التعديل في كل مرة يضيف درجات قبل الترحيل --}}
+                        <input class="save-button " type="submit" value="{{__('public.save')}}">
+                    </div>
+                    <div class="  col mt-1">
+                        <input class="clear-button " type="reset" value="{{__('public.clear')}}">
+                    </div>
                 </div>
-                <div class=" col mt-1">
-                    {{-- زر التعديل في كل مرة يضيف درجات قبل الترحيل --}}
-                    <input class="save-button " type="submit" value="{{__('public.save')}}">
-                </div>
-                <div class="  col mt-1">
-                    <input class="clear-button " type="reset" value="{{__('public.clear')}}">
-                </div>
-            </div>
-        </form>
 @endsection
 
