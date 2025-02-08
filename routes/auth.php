803c8413037ee7230c63978706c910680dfd7d75
AmgadAlhakimy <amgadalhakimy3@gmail.com>
2025-02-08 12:11:43 +0300

    still with progressbar

--- routes/auth.php
+++ routes/auth.php
@@ -18,7 +18,7 @@ Route::middleware('guest')->group(function () {
     Route::get('/', [HomeController::class, 'show'])->name('main_page');
 //    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
 //    Route::post('register', [RegisteredUserController::class, 'store']);
-    Route::get('/register',RegisterUser::class)->name('register');
+    Route::get('/register',RegisterUser::class)->name('register-user');
     Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
